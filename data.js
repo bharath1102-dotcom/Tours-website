0d45254d8cd6ad5a464226a53c2072b3d5a263a5
Bharath-Adhithya <142291131+bharath1102-dotcom@users.noreply.github.com>
2023-10-01 16:19:25 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,51 @@
+import Maldives from "../images/maldives.jpg";
+import Austria from "../images/austria.jpg";
+import Switzerland from "../images/switzerland.jpg";
+import Norway from "../images/norway.jpg";
+import Madagascar from "../images/madagascar.jpg";
+import Bali from "../images/bali.jpg";
+
+export const MenuList = [
+  {
+    name: "Maldives",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Maldives,
+    price: 200000,
+  },
+  {
+    name: "Austria",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Austria,
+    price: 250000,
+  },
+  {
+    name: "Switzerland",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Switzerland,
+    price: 150000,
+  },
+  {
+    name: "Norway",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Norway,
+    price: 150000,
+  },
+  {
+    name: "Madagascar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Madagascar,
+    price: 250000,
+  },
+  {
+    name: "Bali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Bali,
+    price: 100000,
+  },
+];
